ca7ae043cf1574ccc1e3e684a98de4a481a087d9
MarkosBK <markosbasilio17@gmail.com>
2020-11-23 23:45:17 +0200

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lab5',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss',
+    '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+]
 })
 export class AppComponent {
   title = 'ToDoListAng1';
